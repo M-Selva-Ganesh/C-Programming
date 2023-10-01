6f36d80fcafc29a0963b21370e17eb65bfa963ae
M-Selva-Ganesh <146549073+M-Selva-Ganesh@users.noreply.github.com>
2023-10-01 21:35:20 +0530

    Create matrix_sum.c

---
+++ matrix_sum.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    int matrix1[3][3], matrix2[3][3], sum[3][3], i, j;
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            printf("Enter The Value of matrix1[%d][%d] : ", i, j);
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+    
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            printf("Enter The Value of matrix2[%d][%d] : ", i, j);
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++)
+            sum[i][j] = matrix1[i][j] + matrix2[i][j];
+    }
+    
+    
+    printf("Matirx : \n");
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
